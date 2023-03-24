8c700157d651883bae6869b1649fac252e6efa2d
SALAHHAMMAD50 <83323563+SALAHHAMMAD50@users.noreply.github.com>
2023-03-25 00:59:22 +0200

    Initial commit

---
+++ app/src/main/java/com/salahprom/firebaseana/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.salahprom.firebaseana
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var analytics: FirebaseAnalytics
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        analytics = Firebase.analytics
+
+
+    }
+}
